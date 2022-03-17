915e509e42e4722078b99a508098a742f61f608a
lWolvesl <64883691+lWolvesl@users.noreply.github.com>
2022-03-17 20:39:36 +0800

    init

---
+++ chapter2/testIndex.cpp
@@ -0,0 +1,18 @@
+//
+// Created by li on 2022/3/16.
+//
+
+#include "iostream"
+
+using namespace std;
+
+void test(int &x) {
+    x = 1024;
+}
+
+void RunIndex() {
+    int x = 1;
+    cout << x << endl;
+    test(x);
+    cout << x << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "chapter2/testIndex.cpp"
+using namespace std;
+
+#define iniSize 10;
+
+void testIndex(){
+    RunIndex();
+}
+
+int main() {
+    return 0;
+}
