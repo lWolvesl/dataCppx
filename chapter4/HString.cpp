a847d67b6c643ac41a0a0ddd1c01ec697c642129
lWolvesl <64883691+lWolvesl@users.noreply.github.com>
2022-07-15 23:29:36 +0800

    routine

--- chapter4/HString.cpp
+++ chapter4/HString.cpp
@@ -7,30 +7,29 @@
 #include "assert.h"
 
 //串结构
-typedef struct
-{
+typedef struct {
     char *ch;   //指向字符串所在空间首地址
-    int   length; //记录字符串长度
-}HString;
+    int length; //记录字符串长度
+} HString;
 
-void InitHString(HString *s){
+void InitHString(HString *s) {
     s->ch = NULL;
     s->length = 0;
 }
 
-void PrintString(HString *s){
+void PrintString(HString *s) {
     for (int i = 0; i < s->length; ++i) {
-        printf("%c",s->ch[i]);
+        printf("%c", s->ch[i]);
     }
     printf("\n");
 }
 
-void StrAssign(HString *s,char *c){
+void StrAssign(HString *s, char *c) {
     int Clength = strlen(c);
-    if(s->ch != NULL){
+    if (s->ch != NULL) {
         free(s->ch);
     }
-    s->ch = (char*)malloc(sizeof(char) * Clength);
+    s->ch = (char *) malloc(sizeof(char) * Clength);
     assert(s->ch != NULL);
     for (int i = 0; i < Clength; ++i) {
         s->ch[i] = c[i];
@@ -38,11 +37,11 @@ void StrAssign(HString *s,char *c){
     s->length = Clength;
 }
 
-void StrCopy(HString *s,HString *h){
-    if(s->ch != NULL){
+void StrCopy(HString *s, HString *h) {
+    if (s->ch != NULL) {
         free(s->ch);
     }
-    s->ch = (char*)malloc(sizeof(char) * h->length);
+    s->ch = (char *) malloc(sizeof(char) * h->length);
     assert(s->ch != NULL);
     assert(s->ch != NULL);
     for (int i = 0; i < h->length; ++i) {
@@ -51,13 +50,57 @@ void StrCopy(HString *s,HString *h){
     s->length = h->length;
 }
 
-bool StrEmpty(HString *s){
-    if(s->ch == NULL){
+bool StrEmpty(HString *s) {
+    if (s->ch == NULL) {
         return true;
     }
     return false;
 }
 
-int StrCompare(HString *s,HString *h){
+int StrCompare(HString *s, HString *h) {
     return s->length - h->length;
+}
+
+int StrString(HString *s) {
+    return s->length;
+}
+
+void SubString(HString *s, HString *T, int pos, int len) {
+    s->length = len;
+    if (s->ch != NULL) {
+        free(s->ch);
+    }
+    s->ch = (char *) malloc(sizeof(char) * len);
+    assert(s->ch != NULL);
+    for (int i = 0; i < len; ++i) {
+        s->ch[i] = T->ch[pos + i];
+    }
+}
+
+void Concat(HString *S, HString *s1, HString *s2) {
+    S->length = s1->length + s2->length;
+    if (S->ch != NULL) {
+        free(S->ch);
+    }
+    S->ch = (char *) malloc(sizeof(char) * S->length);
+    assert(S->ch != NULL);
+    for (int i = 0; i < s2->length; ++i) {
+        S->ch[i] = s2->ch[s1->length + i];
+    }
+}
+
+//简单模式匹配算法
+int Index(){
+    return 0;
+}
+
+void ClearString(HString *s){
+    if (s->ch != NULL) {
+        free(s->ch);
+    }
+    s->length = 0;
+}
+
+void DestroyString(HString *s){
+    free(s);
 }
